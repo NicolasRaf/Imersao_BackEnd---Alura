1561f499f20a7e3a96dae056c6844b6d20a53e0a
NicolasRaf <nicolasalves136@gmail.com>
2024-11-27 00:23:55 -0300

    Add: att of second class

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 import express from "express";
 
-function main(){
-    const app = express();
-    app.listen(3000, () => console.log("Server Listening..."));
+// Base de Dados
+const posts = [
+    {
+        id: 1,
+        description: "Teste",
+        image: "https://media.licdn.com/dms/image/v2/C4D0BAQFaQ2rt_kVihw/company-logo_200_200/company-logo_200_200/0/1631334580995?e=2147483647&v=beta&t=0gGAf20vE60FAOpZQKZDxmtTdGg7jkP3CdYLVtk5-VE"
+    },
+    {
+        id: 2,
+        description: "Novo Post 1",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        id: 3,
+        description: "Novo Post 2",
+        image: "https://via.placeholder.com/300"
+    },
+    {
+        id: 4,
+        description: "Novo Post 3",
+        image: "https://via.placeholder.com/400"
+    },
+    {
+        id: 5,
+        description: "Novo Post 4",
+        image: "https://via.placeholder.com/500"
+    }
+];
 
-    app.get("/api", (req, res) => res.status(200).send("Hello World!"));
+
+function getPost(id) {
+    return posts.findIndex((post) => post.id === Number(id));
+}
+
+function main() {
+
+    const app = express();
+    app.use(express.json());
+
+    // Rota para obter todos os posts
+    app.get("/posts", (req, res) => res.status(200).json(posts));
+
+    // Rota para obter posts específicos
+    app.get("/posts/:id", (req, res) => {
+        const index = getPost(req.params.id);
+        res.status(200).json(posts[index]);
+    });
+
+    // Inicia o servidor
+    app.listen(3000, () => console.log("Server Listening..."));
 }
 
 main();
